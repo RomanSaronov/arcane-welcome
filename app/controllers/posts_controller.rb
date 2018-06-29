f6b42d505bbe822be4eda509875ecbdcd1867e37
Fromou <fromou@gmail.com>
2018-06-29 23:34:29 +0300

    Posts implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def create
     @posts = Post.new(post_params)
+    if @posts.save
+      redirect_to @posts
+    else
+      render "new"
+    end
   end
+
   private
   def post_params
     params.require(:posts).permit(:name, :price, :duration, :interest)

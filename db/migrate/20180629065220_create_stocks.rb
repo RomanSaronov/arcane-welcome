b68c22f18d185eecfde96e35940ba04894152599
Fromou <fromou@gmail.com>
2018-06-29 10:27:26 +0300

    Stock migration

--- db/migrate/20180629065220_create_stocks.rb
+++ db/migrate/20180629065220_create_stocks.rb
@@ -1,7 +1,10 @@
 class CreateStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :stocks do |t|
-
+      t.string :name
+      t.decimal :price
+      t.decimal :interest
+      t.integer :duration
       t.timestamps
     end
   end

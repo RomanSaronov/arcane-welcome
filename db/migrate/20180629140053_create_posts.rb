2576ef964accf2c2c905625e035310393c59279d
Fromou <fromou@gmail.com>
2018-06-29 20:40:28 +0300

    Posts implementation

--- db/migrate/20180629140053_create_posts.rb
+++ db/migrate/20180629140053_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :name
-      t.decimal :price
-      t.decimal :interest
-      t.integer :duration
+      t.string :name, null: false, default: ""
+      t.decimal :price, null: false, default: 0
+      t.decimal :interest, null: false, default: 0
+      t.integer :duration, null: false, default: 0
       t.belongs_to :user, index: true
       t.timestamps
     end

d6463d82d8ae002d1701cad930e6cdb42930a06a
Fromou <fromou@gmail.com>
2018-06-30 00:35:42 +0300

    Posts implementation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   root to: 'stock#index'
   get '/add', to: 'stock#add'
-  resources :posts
+
+  namespace :add do
+    resources :posts
+  end
 end

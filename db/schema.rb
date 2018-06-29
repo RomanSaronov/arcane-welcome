a83b9e8e0fe3769c0bb553b52e8d044fdcb4b248
Fromou <fromou@gmail.com>
2018-06-29 19:58:24 +0300

    Posts implementation

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2018_06_29_140053) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.decimal "interest"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

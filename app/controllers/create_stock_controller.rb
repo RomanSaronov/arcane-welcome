0bf874b522d50a247df4fb8f157a8afc00858ffa
Fromou <fromou@gmail.com>
2018-06-29 18:26:28 +0300

    Posts implementation

--- app/controllers/create_stock_controller.rb
+++ app/controllers/create_stock_controller.rb
@@ -1,17 +1,3 @@
 class CreateStockController < ApplicationController
-  # def create
-  #   @createstocks = CreateStocks.new(stock_params)
-  #
-  #   if @reatestocks.save
-  #     flash[:notice] = "Stock was created."
-  #     redirect_to 'stock/index'
-  #   else
-  #     render :'stock/add'
-  #   end
-  # end
-  #
-  # private
-  # def stock_params
-  #   params.require(:stocks).permit(:name, :price, :interest, :duration)
-  # end
+
 end

29c19bd7124b95423bd6058fffa3bd2c0d914749
Fromou <fromou@gmail.com>
2018-06-29 18:11:55 +0300

    Login form styles

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,5 +1,6 @@
 class StockController < ApplicationController
   def index
+    @posts = Post.limit(5)
   end
 
   def add
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_065220) do
+ActiveRecord::Schema.define(version: 2018_06_29_140053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.decimal "interest"
+    t.integer "duration"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"

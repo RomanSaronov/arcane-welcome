8fc1a663c7aa33ad3c0f44c2032cf889358dee31
Fromou <fromou@gmail.com>
2018-07-01 21:39:38 +0300

    Posts implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def create
     @posts = Post.new(post_params)
-    if @posts.save!
-      redirect_to root_path,
-      flash[:success] = "Stock is created successfully!"
+    if @posts.save
+      redirect_to root_path, alert: "Stock is created successfully!"
     else
-      redirect_to root_path,
-      flash[:error] = "Something goes wrong. Please try again!"
+      redirect_to root_path, alert: "Something goes wrong. Please try again!"
     end
   end
 

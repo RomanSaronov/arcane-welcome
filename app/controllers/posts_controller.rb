aaa0d46529af0f294927f6cc3262c20f07b2b535
Fromou <fromou@gmail.com>
2018-07-02 02:52:59 +0300

    Posts implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,6 @@ class PostsController < ApplicationController
     @posts = Post.find(params[:id])
   end
 
-  def destroy
-    @posts = Post.find(params[:id])
-    if @posts.destroy
-      redirect_to root_path
-    end
-  end
-
   private
   def post_params
     params.require(:posts).permit(:name, :price, :duration, :interest)

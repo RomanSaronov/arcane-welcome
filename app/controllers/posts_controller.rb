8eec7e7d2779907edc23281d483b104141c9119a
Fromou <fromou@gmail.com>
2018-07-01 17:29:21 +0300

    Posts implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,9 @@
 class PostsController < ApplicationController
   def create
     @posts = Post.new(post_params)
-    if @posts.save
-      redirect_to 'stock/index'
-    else
-      render "new"
-    end
   end
 
+  private
   def post_params
     params.require(:posts).permit(:name, :price, :duration, :interest)
   end

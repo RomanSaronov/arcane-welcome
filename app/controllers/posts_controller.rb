2bcd6e477b77c085f5fd9ea25c37f9a42e42582a
Fromou <fromou@gmail.com>
2018-07-01 17:00:07 +0300

    Posts implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < ApplicationController
+  def create
+    @posts = Post.new(post_params)
+    if @posts.save
+      redirect_to 'stock/index'
+    else
+      render "new"
+    end
+  end
 
+  def post_params
+    params.require(:posts).permit(:name, :price, :duration, :interest)
+  end
 end
--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -4,17 +4,5 @@ class StockController < ApplicationController
   end
 
   def posts
-    def create
-      @posts = Post.new(post_params)
-      if @posts.save
-        redirect_to 'stock/index'
-      else
-        render "new"
-      end
-    end
-
-    def post_params
-      params.require(:posts).permit(:name, :price, :duration, :interest)
-    end
   end
 end

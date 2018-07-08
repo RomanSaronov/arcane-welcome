0092e9c32bc45cc9673cd48912ceed2713398c75
Fromou <fromou@gmail.com>
2018-07-08 19:56:15 +0300

    Project refactoring

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,8 @@
 class Stock < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :duration, presence: true
+  validates :interest, presence: true
 end

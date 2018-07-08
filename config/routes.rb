10d3aff0335991001758428427edc073ef512a42
Fromou <fromou@gmail.com>
2018-07-08 17:05:41 +0300

    Project refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   root 'stock#index'
+  post '/stock/new', to: 'stock#create'
+
   resources :stock
 end

e3808507dcb4d0e5d5cb743b94357fadb7f5043a
Fromou <fromou@gmail.com>
2018-07-08 15:29:49 +0300

    Interest chart implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-  root to: 'stock#index'
-  get '/stock', to: 'stock#index'
 
+  root to: 'stock#index'
   resources :stock
 end

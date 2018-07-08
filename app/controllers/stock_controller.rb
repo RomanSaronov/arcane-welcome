2ead66489afd33181a241368d7d5b90c8021bf96
Fromou <fromou@gmail.com>
2018-07-08 14:54:19 +0300

    Interest chart implementation

---
+++ app/controllers/stock_controller.rb
@@ -0,0 +1,24 @@
+class StockController < ApplicationController
+  def index
+    @stocks = Stock.all
+  end
+
+  def create
+    @stocks = Stock.new(stock_params)
+
+    if @stocks.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @stocks = Stock.find(params[:id])
+  end
+
+  private
+  def stock_params
+    params.require(:stock).permit(:name, :price, :duration, :interest)
+  end
+end

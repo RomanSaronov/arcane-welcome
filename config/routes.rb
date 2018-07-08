8e6adaa1ea2b28554f3cd8cea54b997ea0e70be3
Fromou <fromou@gmail.com>
2018-07-08 16:29:13 +0300

    Project refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
-  root to: 'stock#index'
+  root 'stock#index'
   resources :stock
 end

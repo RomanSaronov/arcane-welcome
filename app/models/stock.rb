e28216ebbb160f1f3beac38ee25b4e1d75e04f6b
Fromou <fromou@gmail.com>
2018-07-09 14:32:52 +0300

    Roles fix

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
 
   validates :name, presence: true
   validates :price, :duration, :interest, presence: true, numericality: { only_integer: true, greater_than: 0 }

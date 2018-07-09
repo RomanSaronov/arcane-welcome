47dd569208ca4c57d8b958918d23f0f21d562f45
Fromou <fromou@gmail.com>
2018-07-09 03:03:32 +0300

    Navigation bar fix

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :price, :duration, :interest, presence: true, numericality: { only_integer: true }
 end

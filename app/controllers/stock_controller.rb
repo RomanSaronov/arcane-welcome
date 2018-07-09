336e3452ed32634a80bd1a0845e0810ff76bc46d
Fromou <fromou@gmail.com>
2018-07-09 15:14:42 +0300

    Roles fix

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -19,6 +19,10 @@ class StockController < ApplicationController
 
   def show
     @stock = Stock.find(params[:id])
+
+    if current_user != @user
+      redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
+    end
   end
 
   private

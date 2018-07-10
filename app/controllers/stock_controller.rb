49df47b07b31020a67af33d6e18cd98ba0e159de
Fromou <fromou@gmail.com>
2018-07-11 00:52:47 +0300

    Stock controller refactor

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -9,6 +9,7 @@ class StockController < ApplicationController
 
   def create
     @stock = Stock.new(stock_params)
+    @stock.user = current_user
     if @stock.save
       flash[:success] = 'Stock added successfully'
       redirect_to root_path
